5e8bba38c0de6d4716adb1854c0f30f57d1c38df
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-08 06:46:45 -0600

    GifsApp aplicacion para buscar imagenes

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent {    
+ 
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  constructor( private gifsService: GifsService) {}
+  
+    buscar( ){
+      const valor = this.txtBuscar.nativeElement.value;
+      
+      //Evita agregar valores vacio en la busqueda
+      if ( valor.trim().length === 0){
+        return;
+      }
+      
+      this.gifsService.buscarGifs (valor);
+      this.txtBuscar.nativeElement.value = '';
+    }
+}
